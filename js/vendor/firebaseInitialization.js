6e9a716911df208a38b6a34db464b5ee24c8387c
Terry Kernan <tkernan@safetynetaccess.com>
2023-02-28 12:35:14 +0000

    added config

--- js/vendor/firebaseInitialization.js
+++ js/vendor/firebaseInitialization.js
@@ -1,10 +1,10 @@
 var config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGE_ID"
+  apiKey: "AIzaSyB1GtEBDqvfNHv_O-1Rs_ciu_oo4nBAktw",
+  authDomain: "retro-a5b27.firebaseapp.com",
+  databaseURL: "https://retro-a5b27.firebaseio.com",
+  projectId: "retro-a5b27",
+  storageBucket: "retro-a5b27.appspot.com",
+  messagingSenderId: "1051872305232"
 };
 
 firebase.initializeApp(config);
